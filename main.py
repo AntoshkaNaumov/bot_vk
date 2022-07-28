7263e59f0528ebf707165a0ce13df61fc06ec868
Anton Naumov <anton.naumov@inbox.ru>
2022-07-28 20:47:53 +0300

    update files

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from random import randrange
-
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_bot import VkBot
 
+
 def write_msg(user_id, message):
     """Функция write_msg получает id пользователя ВК <user_id>,
-     которому оно отправит сообщение и собственно само сообщение """
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+     которому оно отправит сообщение и собственно само сообщение"""
+    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),
+                                'keyboard': keyboard.get_keyboard()})
 
 # API-ключ
 token = 'vk1.a.acscNKsdVoIAwaRfAyPAEx2oq4bNmklARhMiC-zsOP8jfdJj9wFcfPYr4GuMRxbYCo4qCGwGilMy2s6kq8puKHcGTrg0ogU2DnlDrJ0ixdXgU-oGMf_asN3K_f7JpQQPJHrscsk0QbKelNZMB35RGIeCJP8VxvsEAtdjFBtO_bmElKv7oqcL8mZc_h9DPIQO'
@@ -19,7 +20,7 @@ vk = vk_api.VkApi(token=token)
 # Работа с сообщениями
 longpoll = VkLongPoll(vk)
 
-print("Server started")
+print('Server started')
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
@@ -27,6 +28,9 @@ for event in longpoll.listen():
             print(f'For me by: {event.user_id}', end='')
 
             bot = VkBot(event.user_id)
+            keyboard = VkKeyboard()
+            keyboard.add_button('Start', VkKeyboardColor.POSITIVE)
+            keyboard.add_button('Stop', VkKeyboardColor.NEGATIVE)
             write_msg(event.user_id, bot.new_message(event.text))
 
             print('Text: ', event.text)
--- vk_bot.py
+++ vk_bot.py
@@ -5,17 +5,17 @@ import requests
 class VkBot:
 
     def __init__(self, user_id):
-        print("Создан объект бота!")
+        print('Создан объект бота!')
         self._USER_ID = user_id
         self._USERNAME = self._get_user_name_from_vk_id(user_id)
 
-        self._COMMANDS = ["ПРИВЕТ", "ПОКА"]
+        self._COMMANDS = ['ПРИВЕТ', 'ПОКА', 'START', 'STOP']
 
     def _get_user_name_from_vk_id(self, user_id):
-        request = requests.get("https://vk.com/id" + str(user_id))
-        bs = bs4.BeautifulSoup(request.text, "html.parser")
+        request = requests.get('https://vk.com/id' + str(user_id))
+        bs = bs4.BeautifulSoup(request.text, 'html.parser')
 
-        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
+        user_name = self._clean_all_tag_from_str(bs.findAll('title')[0])
 
         return user_name.split()[0]
 
@@ -23,14 +23,21 @@ class VkBot:
 
         # Привет
         if message.upper() == self._COMMANDS[0]:
-            return f"Привет-привет, {self._USERNAME}!"
+            return f'Привет-привет, {self._USERNAME}!'
 
         # Пока
         elif message.upper() == self._COMMANDS[1]:
-            return f"Пока-пока, {self._USERNAME}!"
+            return f'Пока-пока, {self._USERNAME}!'
+
+        # Start
+        elif message.upper() == self._COMMANDS[2]:
+            return 'Search start'
+        # Stop
+        elif message.upper() == self._COMMANDS[3]:
+            return 'End of search'
 
         else:
-            return "Не понимаю о чем вы..."
+            return 'Не понимаю о чем вы...'
 
     # Метод для очистки от ненужных тэгов
 
@@ -38,7 +45,7 @@ class VkBot:
     def _clean_all_tag_from_str(string_line):
         """
         Очистка строки stringLine от тэгов и их содержимых
-        :param string_line: Очищаемая строка
+        :param string_line: очищаемая строка
         :return: очищенная строка
         """
         result = ""
